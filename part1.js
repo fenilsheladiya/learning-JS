0706ab2195945fc81c89afbee11dd5a1887da8fb
fenilsheladiya <fenilsheladiya033@gmail.com>
2024-09-21 16:28:20 +0530

    update

--- part1.js
+++ part1.js
@@ -1,3 +1,153 @@
-console.log("hello mr fenil");
 
-console.log("hello mr fenilsheladiya");
+//?variables
+//   var a = 15; //!anytime multiple  variables are allow   
+//   const b = 20;//! in const variable have declare once ,after value have not changable
+//   let c = 30;//!in let once variable have declare ,then that  fix,variable have not change
+//   const a = 20;
+
+
+
+// var age = 18;
+
+// if(age >= 18){
+//   console.log("You are eligible to vote");
+// }else{
+//     console.log("You are not eligible to vote");
+// }
+
+
+//  var a = 1;
+
+//  switch(a){
+
+//     case 1:
+//         s = 15 + 20;
+//         console.log(s);
+//         break;
+//     case 2:
+//         console.log("two");
+//         break;
+//     case 3:
+//         console.log("three");
+//         break;
+//     default:
+//         console.log("invalid input");
+//  }
+
+
+
+//?for
+// for(var i = 0 ; i < 50 ; i++){
+//     console.log("hello ceo");
+// }
+
+//?while
+// let i = 20;
+
+// while(i >= 10 ){
+//     console.log(i);
+//     i--;
+// }
+
+
+//?do while
+// let i = 1;
+
+// do{    first time execute anfer condition true then run otherwise not
+//     console.log(i);
+//     i++;
+// }while(i <= 20);
+
+
+//?normal function
+
+// function name(){
+//     console.log("hello fenil");
+// }
+
+// name();
+
+//?with parameter
+// function name(a){
+//     console.log("hello" + a);
+// }
+
+// name("  mr founder");
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+
+// sum(33,33);
+
+
+//?arrow function
+
+// let sum = (a,b) => a+b; 
+
+// console.log(sum(2,5));
+
+//?arrays
+
+// let student = ["fenil" ,"nirmit","abhi"];
+// console.log(student);
+
+// let num = [33,55,44];
+// console.log(num);
+// console.log(num[2]);
+// console.log(num.length);
+//?
+
+
+//?datatypes
+//! premitive datatype
+//* 1.Number
+    //  let num = 20;
+    //  console.log(typeof num);
+//* 2.string
+    //  let str = "hello world";
+    //  console.log(typeof str);
+//* 3.boolean
+    //  let boo = true;
+    //  console.log(typeof boo);
+//* 4.null
+    // let nul = null;
+    // console.log(typeof nul);
+//* 5.undefined
+    //    let a;
+    //    console.log(a);                                      
+
+//!non premitive datatype
+//* 1.object
+//* 2.array
+//* 3.function
+
+//?type conversion
+// result = 3+"hello";  
+// result = 3-"hello";  
+// result = '3'+"hello";  
+// result = -33.3033;
+// result = parseFloat(result);
+// console.log(result);
+// console.log(typeof result);
+
+//?operators
+
+//!arithmetic operator(+, - , * , / , %)
+
+// let a = 1;
+// console.log(++a); prefix and postfix
+
+//!comparison operator(==, !=,>=,<=)
+// a = 5;
+// b = 10;
+
+// console.log(a==b);
+
+// console.log(a!=b);
+
+// console.log(a>=b);
+
+// console.log(a<=b);
+//!logical operator(a,b,a||b or , a && b and,!a not)
+
